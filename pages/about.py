c4d90ef5c0792c93a6d19eea916f19729397bf23
maanvi <maanvialung21@gmail.com>
2023-07-08 20:51:55 +0530

    housing prediction

---
+++ pages/about.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+
+#create title
+st.title("Housing Price Data Analysis")
+st.subheader("About This App")
+
+st.markdown("""
+This app performs simple data analysis on **House Price** data set.
+* **Python Libraries:**
+    -streamlit
+    -pandas
+    -mathplotlib
+    -numpy
+    -seaborn
+* **Data Source:** [Kaggle](https://www.kaggle.com/datasets/yasserh/housing-prices-dataset)
+
+<img src="https://c1.wallpaperflare.com/preview/107/911/721/landscape-nature-sunset-of-course.jpg">
+""",unsafe_allow_html=True)
+
